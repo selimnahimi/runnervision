57fe8f01f4d13bb4ffe6a2bfe6e66e055e11925f
Selim Nahimi <selimnahimi@gmail.com>
2023-08-28 20:45:33 +0200

    Refactored vaulting logic
    - Extracted logic to separate methods

--- code/pawn/PawnController.Vaulting.cs
+++ code/pawn/PawnController.Vaulting.cs
@@ -5,6 +5,9 @@ namespace RunnerVision;
 
 public partial class PawnController
 {
+	float showDebugTime => 3f;
+	float boxRadius => 20f;
+
 	bool IsVaulting()
 	{
 		return Vaulting != 0;
@@ -41,8 +44,6 @@ public partial class PawnController
 
 		float rayDistance = Math.Max( (speed / 500) * 60f, 35f );
 
-		float showDebugTime = 3f;
-		float boxRadius = 20f;
 		var distanceBehindObstacle = rayDistance * 1.20f + 60f;
 
 		BBox boxFront = new BBox( center: 0, size: 35f )
@@ -93,94 +94,34 @@ public partial class PawnController
 
 		if ( !traceBehindObstacle.Hit && !hitFailsafe )
 		{
-			// Vault over
-
-			// Check if there's space to vault over
-			var topBoxSmall = new BBox(
-				mins: Vector3.Forward * +boxRadius * 0.70f + Vector3.Up * 50f + Vector3.Left * boxRadius * 0.70f,
-				maxs: Vector3.Forward * -boxRadius * 0.70f + Vector3.Up * 80f + Vector3.Right * boxRadius * 0.70f
-			).Translate( Entity.Position + Entity.Rotation.Forward * rayDistance );
-
-			var traceBoxSmallAboveObstacle = Trace.Box(
-				bbox: topBoxSmall,
-				from: 0, to: 0
-			).Run();
-
-			if ( debugMode )
-				DebugOverlay.Box( bounds: topBoxSmall, color: Color.Green, duration: showDebugTime );
-
-			if ( traceBoxSmallAboveObstacle.Hit )
-				return;
-
-			// Cast a ray to check where the ground is
-			var traceObstacleSurface = Trace.Ray(
-				from: boxBehindObstacle.Center + Entity.Rotation.Up * 30f,
-				to: boxBehindObstacle.Center + Entity.Rotation.Up * -50f
-			).Run();
-
-			if ( debugMode )
-				DebugOverlay.Line(
-					start: boxBehindObstacle.Center + Entity.Rotation.Up * 30f,
-					end: boxBehindObstacle.Center + Entity.Rotation.Up * -50f,
-					color: Color.Blue, duration: showDebugTime
-				);
-
-			if ( traceObstacleSurface.Hit )
-			{
-				var groundPosition = traceObstacleSurface.HitPosition;
-
-				VaultTargetPos = groundPosition;
-			}
+			if ( CanVaultOver( rayDistance ) )
+				VaultOver( rayDistance, boxBehindObstacle );
 			else
 			{
-				VaultTargetPos = boxBehindObstacle.Center + Entity.Rotation.Up * -40f;
+				return;
 			}
-
-			Vaulting = 2;
-			vaultSpeed = 200f;
 		}
 		else
 		{
-			// Vault onto
-
 			// Make sure there's enough space to stand on obstacle
 			var topBoxLarge = new BBox(
 				mins: Vector3.Forward * +boxRadius + Vector3.Up * 45f + Vector3.Left * boxRadius,
 				maxs: Vector3.Forward * -boxRadius + Vector3.Up * 120f + Vector3.Right * boxRadius
 			).Translate( Entity.Position + Entity.Rotation.Forward * rayDistance );
 
-			var traceBoxLargeAboveObstacle = Trace.Box(
-				bbox: topBoxLarge,
-				from: 0, to: 0
-			).Run();
-
-			if ( debugMode )
-				DebugOverlay.Box( bounds: topBoxLarge, color: Color.Green, duration: showDebugTime );
-
-			if ( traceBoxLargeAboveObstacle.Hit )
+			if ( !CanVaultOnto( rayDistance, topBoxLarge ) )
 				return;
 
-			// Cast a ray to check where the ground is
-			var traceObstacleSurface = Trace.Ray(
-				from: topBoxLarge.Center + Entity.Rotation.Up * 30f,
-				to: topBoxLarge.Center + Entity.Rotation.Up * -60f
-			).Run();
+			var groundPosition = VaultOntoGetGround( topBoxLarge );
 
-			if ( debugMode )
-				DebugOverlay.Line(
-					start: topBoxLarge.Center + Entity.Rotation.Up * 30f,
-					end: topBoxLarge.Center + Entity.Rotation.Up * -60f,
-					color: Color.Blue, duration: showDebugTime
-				);
-
-			if ( !traceObstacleSurface.Hit )
+			if ( groundPosition != Vector3.Zero )
+			{
+				VaultOnto( groundPosition );
+			}
+			else
+			{
 				return;
-
-			var groundPosition = traceObstacleSurface.HitPosition;
-
-			VaultTargetPos = groundPosition + Vector3.Up * 13f;
-			Vaulting = 1;
-			vaultSpeed = Math.Max( Entity.Velocity.Length, 200f );
+			}
 		}
 
 		parkouredSinceJumping = true;
@@ -193,5 +134,95 @@ public partial class PawnController
 		var speedAfterVault = Entity.Velocity.WithZ( 0 ).Length;
 
 		Entity.Velocity = vaultDirection * speedAfterVault;
+	}
+
+	bool CanVaultOver( float rayDistance )
+	{
+		// Check if there's space to vault over
+		var topBoxSmall = new BBox(
+			mins: Vector3.Forward * +boxRadius * 0.70f + Vector3.Up * 50f + Vector3.Left * boxRadius * 0.70f,
+			maxs: Vector3.Forward * -boxRadius * 0.70f + Vector3.Up * 80f + Vector3.Right * boxRadius * 0.70f
+		).Translate( Entity.Position + Entity.Rotation.Forward * rayDistance );
+
+		var traceBoxSmallAboveObstacle = Trace.Box(
+			bbox: topBoxSmall,
+			from: 0, to: 0
+		).Run();
+
+		if ( debugMode )
+			DebugOverlay.Box( bounds: topBoxSmall, color: Color.Green, duration: showDebugTime );
+
+		return !traceBoxSmallAboveObstacle.Hit;
+	}
+
+	void VaultOver( float rayDistance, BBox boxBehindObstacle )
+	{
+		// Cast a ray to check where the ground is
+		var traceObstacleSurface = Trace.Ray(
+			from: boxBehindObstacle.Center + Entity.Rotation.Up * 30f,
+			to: boxBehindObstacle.Center + Entity.Rotation.Up * -50f
+		).Run();
+
+		if ( debugMode )
+			DebugOverlay.Line(
+				start: boxBehindObstacle.Center + Entity.Rotation.Up * 30f,
+				end: boxBehindObstacle.Center + Entity.Rotation.Up * -50f,
+				color: Color.Blue, duration: showDebugTime
+			);
+
+		if ( traceObstacleSurface.Hit )
+		{
+			var groundPosition = traceObstacleSurface.HitPosition;
+
+			VaultTargetPos = groundPosition;
+		}
+		else
+		{
+			VaultTargetPos = boxBehindObstacle.Center + Entity.Rotation.Up * -40f;
+		}
+
+		Vaulting = 2;
+		vaultSpeed = 200f;
+	}
+
+	bool CanVaultOnto( float rayDistance, BBox topBoxLarge )
+	{
+		var traceBoxLargeAboveObstacle = Trace.Box(
+			bbox: topBoxLarge,
+			from: 0, to: 0
+		).Run();
+
+		if ( debugMode )
+			DebugOverlay.Box( bounds: topBoxLarge, color: Color.Green, duration: showDebugTime );
+
+		return !traceBoxLargeAboveObstacle.Hit;
+	}
+
+	Vector3 VaultOntoGetGround( BBox topBoxLarge )
+	{
+		// Cast a ray to check where the ground is
+		var traceObstacleSurface = Trace.Ray(
+			from: topBoxLarge.Center + Entity.Rotation.Up * 30f,
+			to: topBoxLarge.Center + Entity.Rotation.Up * -60f
+		).Run();
+
+		if ( debugMode )
+			DebugOverlay.Line(
+				start: topBoxLarge.Center + Entity.Rotation.Up * 30f,
+				end: topBoxLarge.Center + Entity.Rotation.Up * -60f,
+				color: Color.Blue, duration: showDebugTime
+			);
+
+		if ( !traceObstacleSurface.Hit )
+			return Vector3.Zero;
+
+		return traceObstacleSurface.HitPosition;
+	}
+
+	void VaultOnto( Vector3 groundPosition )
+	{
+		VaultTargetPos = groundPosition + Vector3.Up * 13f;
+		Vaulting = 1;
+		vaultSpeed = Math.Max( Entity.Velocity.Length, 200f );
 	}
 }
